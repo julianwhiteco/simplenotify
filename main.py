2896af2f60119515b9b84ed07fdcbc376a20f9f9
Julian <julian@hivemind.consulting>
2023-06-11 01:16:28 +1000

    Build 230611 - Input error handling.
    Added error handling on notification_text function; added Darwin support (testing tba) to both text & buttons notifications.

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from platform import system
 
 
 def notification_text(title, body=None):
+    if title is None:
+        print("No title or data provided. Closed.")
+        exit(1)
+
     if 'Windows' in system():
         from src.windows.client import NotificationClass
         notifier = NotificationClass('Your App Name Here')  # Add this as a function arg.
         return notifier.toast(title, body)
-#    elif 'Darwin' in system():
-#        from src.macos.client import ...
-#    elif 'Linux' in system():
+
+    if 'Darwin' in system():
+        from src.macos.client import MacOSNotification
+        toast = MacOSNotification('Hello', 'Subtitle', body, sound=False)
+        toast.send()
+
+#    if 'Linux' in system():
 #        from src.linux.client import ...
 
 
@@ -26,8 +34,12 @@ def notification_button(title, body, button1, argument1, button2=None, argument2
         notifier = NotificationClass('Your App Name Here')  # Add this as a function arg.
         return notifier.toast(title, body, buttons=buttons)
 
-    # if 'Darwin' in system():
-    #     from src.macos.client import ...
-    #     if button2 is not None and argument2 is not None:
-    #         print("Second notification button is not currently supported by MacOS, ignored.")
+    if 'Darwin' in system():
+        from src.macos.client import MacOSNotification
+        if button2 is not None and argument2 is not None:
+            print("Second notification button is not currently supported by MacOS, ignored.")
+        interactive = MacOSNotification('Hello', 'Subtitle', 'This is an interactive notification', sound=False, action_button_text='View More')
+        interactive.send()
 
+    # if 'Linux' in system():
+    #     from src.linux.client import ...
--- src/macos/client.py
+++ src/macos/client.py
@@ -1,28 +1,28 @@
-# import objc
-# from Foundation import NSUserNotification, NSUserNotificationCenter, NSUserNotificationDefaultSoundName
-#
-#
-# # Note, the following uses the deprecated NSUserNotification.
-# class MacOSNotification:
-#     def __init__(self, title, subtitle, info_text, sound=True, action_button_text=None):
-#         self.title = title
-#         self.subtitle = subtitle
-#         self.info_text = info_text
-#         self.sound = sound
-#         self.action_button_text = action_button_text
-#
-#     def send(self):
-#         notification = NSUserNotification.alloc().init()
-#         notification.setTitle_(self.title)
-#         notification.setSubtitle_(self.subtitle)
-#         notification.setInformativeText_(self.info_text)
-#
-#         if self.action_button_text:
-#             notification.setActionButtonTitle_(self.action_button_text)
-#             notification.set_hasActionButton_(True)
-#
-#         if self.sound:
-#             notification.setSoundName_(NSUserNotificationDefaultSoundName)
-#
-#         center = NSUserNotificationCenter.defaultUserNotificationCenter()
-#         center.scheduleNotification_(notification)
+import objc
+from Foundation import NSUserNotification, NSUserNotificationCenter, NSUserNotificationDefaultSoundName
+
+
+# Note, the following uses the deprecated NSUserNotification.
+class MacOSNotification:
+    def __init__(self, title, subtitle, info_text, sound=True, action_button_text=None):
+        self.title = title
+        self.subtitle = subtitle
+        self.info_text = info_text
+        self.sound = sound
+        self.action_button_text = action_button_text
+
+    def send(self):
+        notification = NSUserNotification.alloc().init()
+        notification.setTitle_(self.title)
+        notification.setSubtitle_(self.subtitle)
+        notification.setInformativeText_(self.info_text)
+
+        if self.action_button_text:
+            notification.setActionButtonTitle_(self.action_button_text)
+            notification.set_hasActionButton_(True)
+
+        if self.sound:
+            notification.setSoundName_(NSUserNotificationDefaultSoundName)
+
+        center = NSUserNotificationCenter.defaultUserNotificationCenter()
+        center.scheduleNotification_(notification)
